765bbcd47afcd40c609bcac25e17c6bc82dfc156
ozgebektas <ozgebektas53@gmail.com>
2023-08-02 17:03:38 +0300

    Add project files.

---
+++ Casgem_SignalR_ChatProject/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Casgem_SignalR_ChatProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
